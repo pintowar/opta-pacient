b2ef61790f62bdb4cec0f70adcce97fee0a0d647
Thiago Oliveira <pintowar@gmail.com>
2019-02-28 23:08:15 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+subprojects {
+    version = "0.5.1"
+    group = "com.github.pintowar"
+
+    apply(plugin = "idea")
+
+    repositories {
+        mavenLocal()
+        jcenter()
+        mavenCentral()
+    }
+}
---
+++ client/build.gradle.kts
@@ -0,0 +1,40 @@
+import com.moowork.gradle.node.yarn.YarnTask
+
+plugins {
+    id("com.moowork.node") version "1.2.0"
+}
+
+project.buildDir = file("dist")
+
+node {
+    version = "8.14.0"
+    yarnVersion = "1.12.3"
+    download = true
+}
+
+tasks.create<YarnTask>("bootRun") {
+    dependsOn("yarn")
+    group = "application"
+    description = "Run the client app"
+    args = listOf("run", "serve")
+}
+
+tasks.create<YarnTask>("build") {
+    dependsOn("yarn")
+    group = "build"
+    description = "Build the client bundle"
+    args = listOf("run", "build")
+}
+
+tasks.create<YarnTask>("clean") {
+    group = "build"
+    description = "Clean the client bundle"
+    delete("dist")
+}
+
+tasks.create<YarnTask>("test") {
+    dependsOn("yarn")
+    group = "verification"
+    description = "Run the client tests"
+    args = listOf("run", "test")
+}
